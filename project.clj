30f56693fbb30caeabf4e4b7d82643ee2c2f99ed
Casey Marshall <csm@noonhome.com>
2019-06-10 14:04:38 -0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/lore "0.1.0-SNAPSHOT"
+(defproject com.github.csm/lore "0.1.0"
   :description "Secret storage API for clojure"
   :url "https://github.com/csm/lore"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
